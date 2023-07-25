565820ee9fc0749483e2625d3c8b7b6c50cf041f
Iqbal Hossain <iqbalgazi2021@gmail.com>
2023-07-26 00:08:32 +0600

    Add Meta & description Style Section

--- widgets/blog-post-widget.php
+++ widgets/blog-post-widget.php
@@ -254,6 +254,89 @@ class Blog_post_Widget extends \Elementor\Widget_Base {
 		$this->end_controls_section();
 		// Blog Post Wrapper Style Section End
 
+
+		// Blog Post Thumb Style Section Start
+		$this->start_controls_section(
+			'ele_element_blog_post_thumb',
+			[
+				'label' => esc_html__( 'Thumb', 'elementrio' ),
+				'tab' => \Elementor\controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_thumb_width',
+			[
+				'label' => esc_html__( 'Width', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px' ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 600,
+						'step' => 1,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .elementrio-post-thumb' => 'width: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_thumb_height',
+			[
+				'label' => esc_html__( 'Height', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px' ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 600,
+						'step' => 1,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .elementrio-post-thumb' => 'height: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'ele_element_blog_post_thumb_border',
+				'selector' => '{{WRAPPER}} .ele-element .elementrio-post-thumb',
+			]
+		);
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_thumb_radius',
+			[
+				'label' => esc_html__( 'Border Radius', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .elementrio-post-thumb' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_thumb_margin',
+			[
+				'label' => esc_html__( 'Margin', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .elementrio-post-thumb' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+		// Blog Post Thumb Style Section End
+
 		// Blog Post Title Style Section Start
 		$this->start_controls_section(
 			'ele_element_blog_post_title',
@@ -309,6 +392,233 @@ class Blog_post_Widget extends \Elementor\Widget_Base {
 
 		$this->end_controls_section();
 		// Blog Post Title Style Section End
+
+		// Blog Post Description Style Section Start
+		$this->start_controls_section(
+			'ele_element_blog_post_description',
+			[
+				'label' => esc_html__( 'Description', 'elementrio' ),
+				'tab' => \Elementor\controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'ele_element_blog_post_description_typography',
+				'selector' => '{{WRAPPER}} .ele-element .post-description',
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_description_color',
+			[
+				'label' => esc_html__( 'Color', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-description' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_description_margin',
+			[
+				'label' => esc_html__( 'Margin', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-description' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+		// Blog Post Description Style Section End
+
+		// Blog Post Meta  Style Section Start
+		$this->start_controls_section(
+			'ele_element_blog_post_meta',
+			[
+				'label' => esc_html__( 'Meta', 'elementrio' ),
+				'tab' => \Elementor\controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'ele_element_blog_post_meta_typography',
+				'selector' => '{{WRAPPER}} .ele-element .post-meta-list .meta-author, {{WRAPPER}} .ele-element .post-meta-list .meta-date, {{WRAPPER}} .ele-element .post-meta-list .post-cat, {{WRAPPER}} .ele-element .post-meta-list .post-comment',
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_meta_icon',
+			[
+				'label' => esc_html__( 'Icon Size', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px' ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author i' => 'font-size: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat i' => 'font-size: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date i' => 'font-size: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment i' => 'font-size: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->start_controls_tabs(
+			'ele_element_blog_post_meta_tabs'
+		);
+
+		$this->start_controls_tab(
+			'ele_element_blog_post_meta_normal_tab',
+			[
+				'label' => esc_html__( 'Normal', 'elementrio' ),
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_meta_color',
+			[
+				'label' => esc_html__( 'Color', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author, a' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat, a' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date, a' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment, a' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Background::get_type(),
+			[
+				'name' => 'ele_element_blog_post_meta_bg',
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .ele-element .post-meta-list .meta-author, {{WRAPPER}} .ele-element .post-meta-list .meta-date, {{WRAPPER}} .ele-element .post-meta-list .post-cat, {{WRAPPER}} .ele-element .post-meta-list .post-comment',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'ele_element_blog_post_meta_border',
+				'selector' => '{{WRAPPER}} .ele-element .post-meta-list .meta-author, {{WRAPPER}} .ele-element .post-meta-list .meta-date, {{WRAPPER}} .ele-element .post-meta-list .post-cat, {{WRAPPER}} .ele-element .post-meta-list .post-comment',
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->start_controls_tab(
+			'ele_element_blog_post_meta_hover_tab',
+			[
+				'label' => esc_html__( 'Hover', 'elementrio' ),
+			]
+		);
+
+		$this->add_control(
+			'ele_element_blog_post_meta_hv_color',
+			[
+				'label' => esc_html__( 'Color', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author:hover, a:hover' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat:hover, a:hover' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date:hover, a:hover' => 'color: {{VALUE}}',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment:hover, a:hover' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Background::get_type(),
+			[
+				'name' => 'ele_element_blog_post_hv_meta_bg',
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .ele-element .post-meta-list .meta-author:hover, {{WRAPPER}} .ele-element .post-meta-list .meta-date:hover, {{WRAPPER}} .ele-element .post-meta-list .post-cat:hover, {{WRAPPER}} .ele-element .post-meta-list .post-comment:hover',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'ele_element_blog_post_meta_hv_border',
+				'selector' => '{{WRAPPER}} .ele-element .post-meta-list .meta-author:hover, {{WRAPPER}} .ele-element .post-meta-list .meta-date:hover, {{WRAPPER}} .ele-element .post-meta-list .post-cat:hover, {{WRAPPER}} .ele-element .post-meta-list .post-comment:hover',
+			]
+		);
+
+		$this->end_controls_tab();
+		$this->end_controls_tabs();
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_meta_radius',
+			[
+				'label' => esc_html__( 'Border Radius', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_meta_padding',
+			[
+				'label' => esc_html__( 'Padding', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'ele_element_blog_post_meta_margin',
+			[
+				'label' => esc_html__( 'Margin', 'elementrio' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-author' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-cat' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .meta-date' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .ele-element .post-meta-list .post-comment' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+		// Blog Post Meta Style Section End
+
+		// Blog Post Button  Style Section Start
+		$this->start_controls_section(
+			'ele_element_blog_post_button',
+			[
+				'label' => esc_html__( 'Button', 'elementrio' ),
+				'tab' => \Elementor\controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->end_controls_section();
+		// Blog Post Button Style Section End
 	}
 
 	protected function render( ) {
@@ -341,8 +651,8 @@ class Blog_post_Widget extends \Elementor\Widget_Base {
 						<div class="col-md-6">
 							<div class="elementrio-post-card">
 								<div class="elementrio-post-header">
-									<a href="<?php the_permalink(); ?>" class="elementrio-post-thumb">
-										<img src="<?php the_post_thumbnail_url( esc_attr( 'large' ) ); ?>" alt="<?php the_title(); ?>">
+									<a href="<?php the_permalink(); ?>">
+										<img src="<?php the_post_thumbnail_url( esc_attr( 'large' ) ); ?>" class="elementrio-post-thumb" alt="<?php the_title(); ?>">
 									</a>
 								</div>
 								<div class="elementrio-post-body ">
@@ -370,7 +680,7 @@ class Blog_post_Widget extends \Elementor\Widget_Base {
 										</span>
 									</div>
 
-									<p><?php echo esc_html( wp_trim_words(get_the_excerpt(), 20) ); ?></p>
+									<p class="post-description"><?php echo esc_html( wp_trim_words(get_the_excerpt(), 20) ); ?></p>
 
 									<div class="btn-wrapper">
 										<a href="<?php the_permalink(); ?>" class="elementrio-post-btn ">
