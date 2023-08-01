61cb726e494ec095f1e5ec42bc67e9d6e82ab101
Iqbal Hossain <iqbalgazi2021@gmail.com>
2023-08-02 00:05:43 +0600

    Drop Cap widget

--- config/widget-lists.php
+++ config/widget-lists.php
@@ -12,3 +12,9 @@ function register_icon_box_widget($widgets_manager) {
     require_once(__DIR__ . '/../widgets/icon-box-widget.php');
     $widgets_manager->register(new \Icon_Box_Widget());
 }
+
+// Register Drop Caps widget
+function register_drop_caps_widget($widgets_manager) {
+    require_once(__DIR__ . '/../widgets/drop-caps-widget.php');
+    $widgets_manager->register(new \Drop_Caps_Widget());
+}
