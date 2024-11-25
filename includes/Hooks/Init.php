49ba733ced84505c9138d500b6a62ff7be562c54
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2024-11-25 19:37:56 +0600

    Update style

---
+++ includes/Hooks/Init.php
@@ -0,0 +1,27 @@
+<?php
+namespace Elementrio\Hooks;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Enqueue registrar.
+ *
+ * @since 1.0.0
+ * @access public
+ */
+class Init {
+
+	use \Elementrio\Traits\Singleton;
+
+	/**
+	 * class constructor.
+	 * private for singleton
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		// Third Party Compatibility
+		//\Gutenkit\Hooks\ThirdPartyCompatibility::instance();
+	}
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementrio/elementrio',
+        'pretty_version' => 'dev-Update-plugin',
+        'version' => 'dev-Update-plugin',
+        'reference' => '1b7ec2ef450a51accbf66fd80c8062a3c3dc7931',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elementrio/elementrio' => array(
+            'pretty_version' => 'dev-Update-plugin',
+            'version' => 'dev-Update-plugin',
+            'reference' => '1b7ec2ef450a51accbf66fd80c8062a3c3dc7931',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
